3b1b57b5ab409dae4dba889b61a0bcb025cf5cdb
kehinde ojo <ojok761@gmail.com>
2024-03-11 04:38:35 -0400

    finished

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { BrowserRouter as Router , Route, Routes} from 'react-router-dom'
+import Contact from "./component/Contact"
+import Home from "./component/Home"
+import About from "./component/About"
+import Nav from "./Nav"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+   <Router>
+    <Nav/>
+    <Routes>
+
+      <Route path='/home' element={<Home/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/about' element={<About/>}/>
+    </Routes>
+
+   </Router>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      hello Contact
+    </div>
+  )
+}
+
+export default Contact
